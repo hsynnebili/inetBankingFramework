9f56a0cfb9ac75764a88de05ba48bdb048425efa
huseyinnebili <huseyinnebili@hotmail.com>
2021-04-18 12:13:32 -0400

    The latest version

--- NopcommerceFramework/src/test/java/runners/Test_Runner.java
+++ NopcommerceFramework/src/test/java/runners/Test_Runner.java
@@ -7,8 +7,12 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "/Users/husey/eclipse-workspace/NopcommerceFramework/Features/Login.feature"
-, glue = "com/NopcommerceFramework/stepDefinitions")
-
+, glue = "com/NopcommerceFramework/stepDefinitions"
+,dryRun=false
+,monochrome=true
+,tags= {"@skill"}    
+,plugin = {"pretty", "html:target/cucumber-default-reports", "json:target/cucumber.json"}
+	)
 public class Test_Runner {
 
 }
